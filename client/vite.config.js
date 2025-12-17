6765eff8c4f624f06e737de7b3b632df30d4f6b7
AwaisShabbir0 <wasikamboh0810@gmail.com>
2025-12-17 10:49:42 +0500

    feat: Remove backend and finalize client for deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,10 +5,10 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()], // mkcert() removed for debugging
+  plugins: [react(), mkcert()],
   server: {
     port: 5173,
-    // https: true,
+    https: true,
   },
   resolve: {
     alias: {
